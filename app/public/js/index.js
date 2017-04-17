b63fd07c71a34193f103870e721e8c88514cbf5b
alexcrist <alexecrist@gmail.com>
2017-04-16 22:58:01 -0400

    Add react front end template

---
+++ app/public/js/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import {render} from 'react-dom';
+
+import Layout from './components/Layout';
+
+const content = document.getElementById('content');
+render(<Layout/>, content);
